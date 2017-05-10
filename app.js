8727c488c49263e42bd11cf27ac8c6e094700104
hexson <1477546184@qq.com>
2017-05-11 01:06:10 +0800

    update api module

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ var users = require('./routes/users');
 
 var app = express();
 
+// Enable All CORS Requests
+// var cors = require('cors');
+// app.use(cors);
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
