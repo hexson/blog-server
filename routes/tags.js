17ce69905a225d136d21c5e5da32b44586e7d356
hadley <1477546184@qq.com>
2017-05-12 18:42:55 +0800

    update tags && classify module

---
+++ routes/tags.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var $u = require('../utils');
+var db = require('../schema');
+var router = express.Router();
+
+
+router.post('/get', function(req, res, next) {
+  db.Tag.find({}, {_id: 0}, function(err, doc){
+    if (err) return res.send(err);
+    res.send(doc);
+  })
+});
+
+router.post('/create', function(req, res, next) {
+  let { name, auth } = req.body;
+  if (auth != '123456') return res.send({code: 0, msg: '非法请求'});
+  if (!name) return res.send({code: 0, msg: '标签名不能为空'});
+  db.$id.findOneAndUpdate({table_name: 'tags'}, {$inc: {sequence_value: 1}}, {new: true}, function(err, doc){
+    if (err) return res.send(err);
+    let tags = new db.Tag({
+      id: doc.sequence_value,
+      name: name,
+      updated_at: null,
+      created_at: $u.getTime()
+    });
+    tags
+    .save()
+    .then((r) => {
+      res.send(r);
+    })
+    .catch((e) => {
+      res.send(e);
+    })
+  })
+});
+
+module.exports = router;
