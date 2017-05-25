82c977a6db0cf25c861d27f4187db4e58fd518e4
hadley <1477546184@qq.com>
2017-05-25 20:10:56 +0800

    update api module

--- routes/admin/posts.js
+++ routes/admin/posts.js
@@ -41,29 +41,32 @@ router.get('/get/:id', function(req, res, next) {
 });
 
 router.post('/add', function(req, res, next) {
-  let { title, classify, tags, preview, body } = req.body;
+  let { title, classify, tags, preview, body, created_at } = req.body;
   if (!title) return res.send({code: 0, msg: '文章标题不能为空'});
   if (!classify) return res.send({code: 0, msg: '文章分类不能为空'});
   if (!tags) return res.send({code: 0, msg: '文章标签不能为空'});
   if (!preview) return res.send({code: 0, msg: '文章快照不能为空'});
   if (!body) return res.send({code: 0, msg: '文章内容不能为空'});
+  created_at = Math.floor(new Date(created_at).getTime() / 1000);
+  let data = {
+    title: title,
+    classify: classify,
+    tags: tags,
+    preview: preview,
+    body: body,
+    views: 0,
+    show: 1,
+    updated_at: null,
+    created_at: created_at
+  };
+  if (isNaN(created_at) || !created_at || created_at.toString().length !== 10) data.created_at = $u.now();
   db.Post.count({title: title}, function(e, n){
     if (e) return res.send(e);
     if (n > 0) return res.send({code: 0, msg: '文章标题已经存在'});
     db.$id.findOneAndUpdate({table_name: 'posts'}, {$inc: {sequence_value: 1}}, {new: true}, function(err, doc){
       if (err) return res.send(err);
-      let post = new db.Post({
-        id: doc.sequence_value,
-        title: title,
-        classify: classify,
-        tags: tags,
-        preview: preview,
-        body: body,
-        views: 0,
-        show: 1,
-        updated_at: null,
-        created_at: $u.now()
-      });
+      data.id = doc.sequence_value;
+      let post = new db.Post(data);
       post.save()
       .then((r) => {
         res.send({
@@ -80,26 +83,30 @@ router.post('/add', function(req, res, next) {
 
 router.post('/update/:id', function(req, res, next) {
   let { id } = req.params;
-  let { title, classify, tags, preview, body } = req.body;
+  let { title, classify, tags, preview, body, created_at } = req.body;
   if (!title) return res.send({code: 0, msg: '文章标题不能为空'});
   if (!classify) return res.send({code: 0, msg: '文章分类不能为空'});
   if (!tags) return res.send({code: 0, msg: '文章标签不能为空'});
   if (!preview) return res.send({code: 0, msg: '文章快照不能为空'});
   if (!body) return res.send({code: 0, msg: '文章内容不能为空'});
+  created_at = Math.floor(new Date(created_at).getTime() / 1000);
+  let data = {
+    title: title,
+    classify: classify,
+    tags: tags,
+    preview: preview,
+    body: body,
+    updated_at: $u.now(),
+    created_at: created_at
+  };
+  if (isNaN(created_at) || !created_at || created_at.toString().length !== 10) delete data.created_at;
   db.Post.count({id: id}, function(e, n){
     if (e) return res.send(e);
     if (n <= 0) return res.send({code: 0, msg: '文章不存在'});
     db.Post.find({id: {$ne: id}, title: title}, function(er, d){
       if (er) return res.send(er);
       if (d.length > 0) return res.send({code: 0, msg: '文章标题已经存在'});
-      db.Post.updateOne({id: id}, {$set: {
-        title: title,
-        classify: classify,
-        tags: tags,
-        preview: preview,
-        body: body,
-        updated_at: $u.now()
-      }}, function(err, result){
+      db.Post.updateOne({id: id}, {$set: data}, function(err, result){
         if (err) return res.send(err);
         if (result.n > 0) res.send({code: 1, msg: '文章更新成功'});
         else res.send({code: 0, msg: '文章更新失败'});
